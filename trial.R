c6ab56000844288babfefc0505d6506e1e8ca62f
nfox29 <nf2g13@soton.ac.uk>
2019-10-08 11:20:19 +0100

    - trial.R now includes an quick example

--- trial.R
+++ trial.R
@@ -1,3 +1,11 @@
-#trial code
+#basic example
+library(devtools)
+devtools::install_github("nfox29/photosearcher")
+library(photosearcher)
 
-#new line
+
+photo_meta <- photosearcher::photo_search(mindate_taken = "2018-12-25", 
+                                          tags = c("tree"),
+                                          tags_any = FALSE,
+                                          bbox = "-0.312836,51.439050,-0.005219,51.590237")
+urls <- data.frame(photo_meta$url_o)
