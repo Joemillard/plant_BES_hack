80cc7ef66c65472404d5b4dfa52d359817e8b3cf
Roy Sanderson <roy.sanderson@newcastle.ac.uk>
2019-10-08 14:04:14 +0100

    Added in url_l

--- filetext.R
+++ filetext.R
@@ -1,1 +1,11 @@
-# testfile
+library(devtools)
+devtools::install_github("nfox29/photosearcher")
+library(sf)
+library(photosearcher)
+
+boundaries <- sf::st_read(".\\shape\\National_Parks_England.shp")
+
+photo_meta <- photosearcher::photo_search(mindate_taken = "2018-12-25",
+                                        tags = c("tree"),
+                                        tags_any = FALSE,
+                                        bbox = "-0.312836, 51.439050,-0.005219,51,590237")
