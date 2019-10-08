e0bf7b2f3b086e34684294cf2456fa2c6454b3e2
Joemillard <joeymillard@hotmail.co.uk>
2019-10-08 14:16:54 +0100

    commit addition of plantnet

--- app.R
+++ app.R
@@ -1,21 +1,9 @@
 library(shiny)
 library(leaflet)
 library(photosearcher)
+library(plantnet)
 
 ##
-
-#photo_meta <- structure(list(latitude = c(51.524923, 51.534803, 51.53511, 51.528067, 
-#                                 51.542116, 51.564689), longitude = c(-0.10621, -0.10063, -0.13098, 
-#                                                                     -0.066662, -0.036561, -0.054239), URLS = structure(c(6L, 1L, 
-#                                                                                                                         2L, 3L, 4L, 5L), .Label = c("https://images.pexels.com/photos/1103715/pexels-photo-1103715.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260", 
-#                                                                                                                                                    "https://images.pexels.com/photos/1104365/pexels-photo-1104365.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260", 
-#                                                                                                                                                   "https://images.pexels.com/photos/14111/pexels-photo-14111.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260", 
-#                                                                                                                                                  "https://images.pexels.com/photos/358429/pexels-photo-358429.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260", 
-#                                                                                                                                                 "https://images.pexels.com/photos/583850/pexels-photo-583850.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260", 
-#                                                                                                                                                "https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
-#                                                                                                                   ), class = "factor"), tags = c("flowers, London", "flowers", 
-#                                                                                                                                                 "plant", "plant, London", "flowers", "london")), row.names = c(NA, 
-#-6L), class = "data.frame")
 
 ui <- fluidPage(
   titlePanel("Real time plant observations in London"),
@@ -57,7 +45,57 @@ server <- function(input,output,session) {
   })
   
   observeEvent(input$plantnet, {
-    # code for plantnet goes here
+    
+    key <- "2a10egBxTG839st87lMrBoWQ"
+    
+    # imageURL <- 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/60/Single_lavendar_flower02.jpg/800px-Single_lavendar_flower02.jpg'
+    # classifications <- identify(key, imageURL)
+    
+    # Read csv test file of URL's
+    #url_list <- read.csv("photo_search_intial.csv", stringsAsFactors = FALSE)
+    
+    url_list <- photo_meta
+    
+    ids_urls <- url_list[, c("id", "latitude", "longitude", "datetaken", "url_l")]
+    no_of_urls <- nrow(ids_urls)
+    
+    
+    # Need to fix datetaken which comes in as a factor
+    
+    top_output <- data.frame(matrix(NA, nrow=1, ncol=11))
+    colnames(top_output) <- c("id", "latitude", "longitude", "datetaken", "url_l", "prob1", "cname1", "prob2", "cname2", "prob3", "cname3")
+    loop_output <- top_output
+    
+    for(i in 1:no_of_urls){
+      this_url <- data.frame(identify(key, as.character(ids_urls$url_l[i])))
+      loop_output[1, "id"] <- ids_urls[i, 1]
+      loop_output[1, "latitude"] <- ids_urls[i, 2]
+      loop_output[1, "longitude"] <- ids_urls[i, 3]
+      loop_output[1, "datetaken"] <- ids_urls[i, 4]
+      loop_output[1, "url_l"] <- ids_urls[i, 5]
+      if(nrow(this_url) == 1){
+        loop_output[1, "prob1"] <- NA
+        loop_output[1, "cname1"] <- NA
+      }else if(nrow(this_url) == 2){
+        loop_output[1, "prob1"] <- unlist(this_url[1, 1]) # score top
+        loop_output[1, "cname1"] <- unlist(this_url[1, 3]) # common name top
+        loop_output[1, "prob2"] <- unlist(this_url[2, 1]) # 2nd score
+        loop_output[1, "cname2"] <- unlist(this_url[2, 3]) # 2nd name
+      } else if(nrow(this_url) >= 3){
+        loop_output[1, "prob1"] <- unlist(this_url[1, 1]) # score top
+        loop_output[1, "cname1"] <- unlist(this_url[1, 3]) # common name top
+        loop_output[1, "prob2"] <- unlist(this_url[2, 1]) # 2nd score
+        loop_output[1, "cname2"] <- unlist(this_url[2, 3]) # 2nd name
+        loop_output[1, "prob3"] <- unlist(this_url[3, 1]) # 3rd score
+        loop_output[1, "cname3"] <- unlist(this_url[3, 3]) # 3rd name
+      }
+      top_output <- rbind(top_output, loop_output)
+      loop_output <- data.frame(matrix(NA, nrow=1, ncol=11))
+      colnames(loop_output) <- c("id", "latitude", "longitude", "datetaken", "url_l", "prob1", "cname1", "prob2", "cname2", "prob3", "cname3")
+    }
+    top_output <- top_output[-1,]
+    rownames(top_output) <- 1:no_of_urls
+    
   })
   
   observeEvent(input$plot_map, {
